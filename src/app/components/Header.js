24dd8ed85de73e7180eda97786c9f8e0b6f60ce1
Naghmeh Shirazi <naghmehshirazi@naghmehs-mbp.gateway.pace.com>
2016-09-25 17:52:24 -0500

    added the configureStore.js file
    updated the indentations

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,34 +1,29 @@
-import React, {PropTypes, Component} from 'react';
+import React, { PropTypes, Component } from 'react';
 import TodoTextInput from './TodoTextInput';
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSave = this.handleSave.bind(this);
-  }
-
-  handleSave(text) {
-    if (text.length !== 0) {
-      this.props.addTodo(text);
+    constructor(props) {
+        super(props);
+        this.handleSave = this.handleSave.bind(this);
     }
-  }
 
-  render() {
-    return (
-      <header className="header">
-        <h1>todos</h1>
-        <TodoTextInput
-          newTodo
-          onSave={this.handleSave}
-          placeholder="What needs to be done?"
-          />
-      </header>
-    );
-  }
+    handleSave(text) {
+        if (text.length !== 0) {
+            this.props.addTodo(text);
+        }
+    }
+
+    render() {
+        return ( < header className = "header" >
+            < h1 > Yo < /h1> < TodoTextInput newTodo onSave ={this.handleSave}
+            placeholder = "What needs to be done?" / >
+            < /header>
+        );
+    }
 }
 
 Header.propTypes = {
-  addTodo: PropTypes.func.isRequired
+    addTodo: PropTypes.func.isRequired
 };
 
 export default Header;
